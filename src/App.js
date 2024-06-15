d33659f9e829ed5aaa356aa0ac2a033fe30772dd
moacirrochadev <empreendedormoacirneto@gmail.com>
2024-06-14 21:11:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './App.css';
+import { Banner } from './componentes/Banner'
+import { Formulario } from './componentes/Formulario';
+import { Time } from './componentes/Time';
+
+function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#D9F7E9',
+      corSecundaria: '#57C278'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#E8F8FF',
+      corSecundaria: '#82CFFA'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#F0F8E2',
+      corSecundaria: '#A6D157'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#FDE7E8',
+      corSecundaria: '#E06B69'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#FAE9F5',
+      corSecundaria: '#DB6EBF'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFF5D9',
+      corSecundaria: '#FFBA05'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FFEEDF',
+      corSecundaria: '#FF8A29'
+    }
+  ]
+
+const [colaboradores, setColaboradores] = useState([])
+
+const aoNovoColaboradorAdicionado = (colaborador) => {
+  setColaboradores([...colaboradores, colaborador])
+}
+
+  return (
+    <div className="App">  
+    <Banner />
+    <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+    
+    {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,23 @@
+import { Colaborador } from "../Colaborador";
+import "./Time.css";
+
+export const Time = (props) => {
+  const css = { backgroundColor: props.corPrimaria };
+  return (
+    (props.colaboradores.length > 0) &&
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            corDeFundo={props.corSecundaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
